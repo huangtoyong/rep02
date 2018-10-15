96003f74f0b1fcdffa10c796f880b06f75b62f1d
huang <604466360@qq.com>
2018-10-15 11:13:40 +0800

    将itheima推送到rep01

--- itheima/src/com/itheima/C.java
+++ itheima/src/com/itheima/C.java
@@ -4,5 +4,6 @@ public class C {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("245631");
+        System.out.println("123456789");
     }
 }

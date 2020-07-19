ec19843c8968cb2758395b662d7e2fb1ed9f9536
huang <604466360@qq.com>
2020-07-19 19:20:52 +0800

    提交

--- itheima/src/com/itheima/C.java
+++ itheima/src/com/itheima/C.java
@@ -3,7 +3,7 @@ package com.itheima;
 public class C {
     public static void main(String[] args) {
         System.out.println("123456");
-        System.out.println("245631");
+        System.out.println("24563100");
         System.out.println("123456789");
     }
 }
